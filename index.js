1c3f8965fe9cf0a0dffed4d2caef65ea78dd6294
iheathers <shresthapritam7@gmail.com>
2021-06-14 20:58:10 +0545

    my solution to the problem

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ fs.readdir(process.cwd(), async (err, files) => {
     throw new ReferenceError(err);
   }
 
+  const statArray = Array(files.length).fill(null);
+
+  for (const [index, file] of files.entries()) {
+    statArray[index] = lstat(file);
+  }
+
+  const awaitedArray = await Promise.all(statArray);
+
+  const ready = awaitedArray.every((item) => item !== null);
+
+  console.log(awaitedArray);
+
+  if (ready) {
+    console.log('file', files[2], ' statObj ', awaitedArray[2].isFile());
+  }
   // Note: Turns out that forEach works differently on async / await functions
 
   // files.forEach(async (file, index) => {
@@ -18,25 +33,24 @@ fs.readdir(process.cwd(), async (err, files) => {
   //   console.log({ fileType });
   // });
 
-  for (const file of files) {
-    try {
-      const filetype = await lstat(file);
-      console.log(filetype);
-    } catch (err) {
-      console.log(err);
-    }
-  }
+  // for (const file of files) {
+  //   try {
+  //     const filetype = await lstat(file);
+  //     console.log(filetype);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }
 });
 
 // Method 1
 const lstat = (file) => {
   return new Promise((resolve, reject) => {
-    console.log({ file });
     fs.lstat(file, (err, stats) => {
       if (err) {
         reject(err);
       } else {
-        resolve({ [file]: stats.isFile() });
+        resolve(stats);
       }
     });
   });
